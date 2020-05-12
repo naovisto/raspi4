6baa475cfbad3ef9a08d5684e080a7673321e8f3
naovisto <naovisto@Cobalt>
2020-05-12 00:22:44 -0400

    commented out breaking locale and user changes

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -18,21 +18,24 @@ dnsserver=
 
 ## Start ##
 
-# Change default usernames/passwords
+# Change default root username/password
+# Needed to change this bc pkill would
+# (logically) kill the script as well as
+#the ssh session
 
-pkill -9 -u $oldusername
-sudo usermod -l $newusername $oldusername
-passwd $newpassword
+#pkill -9 -u $oldusername
+#sudo usermod -l $newusername $oldusername
+#passwd $newpassword
 
 passwd $newrootpassword
 
 # Set locale
-sudo grep ^en_US\.UTF-8 /usr/share/i18n/SUPPORTED > /var/lib/locales/supported.d/local
-sudo dpkg-reconfigure locales
-sudo dpkg-reconfigure keyboard-configuration
-sudo echo "America/New York" > /etc/timezone
-sudo dpkg-reconfigure -f noninteractive tzdata
-sudo sed -i '/ftp.uk.debian.org/s/uk/us/g' /etc/apt/sources.list
+#sudo grep ^en_US\.UTF-8 /usr/share/i18n/SUPPORTED > /var/lib/locales/supported.d/local
+#sudo dpkg-reconfigure locales
+#sudo dpkg-reconfigure keyboard-configuration
+#sudo echo "America/New York" > /etc/timezone
+#sudo dpkg-reconfigure -f noninteractive tzdata
+#sudo sed -i '/ftp.uk.debian.org/s/uk/us/g' /etc/apt/sources.list
 
 ## Networking ##
 
@@ -62,8 +65,8 @@ printf "\nnet.ipv6.conf.all.disable_ipv6 = 1\n" | sudo tee -a /etc/sysctl.conf
 
 # Uninstall BlueZ and related packages.
 
-Sudo apt-get purge bluez -y
-Sudo apt-get autoremove -y
+sudo apt-get purge bluez -y
+sudo apt-get autoremove -y
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 
